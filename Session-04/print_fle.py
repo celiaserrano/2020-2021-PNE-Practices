3f124083020df5a95481ef0c550fa13016c88ab0
celiaserrano <c.serranos.2020@alumnos.urjc.es>
2021-02-23 17:06:30 +0100

    saving

---
+++ Session-04/print_fle.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+
+# -- Constant with the new of the file to open
+FILENAME = "RNU6_269P.txt"
+
+# -- Open and read the file
+file_contents = Path(FILENAME).read_text()
+
+# -- Print the contents on the console
+print(file_contents)
+
+
+FILENAME = "RNU6_269P.txt"
+
+try:
+    file_contents = Path(FILENAME).read_text()
+    print(file_contents)
+
+except FileNotFoundError:
+    print("Something wrong has happened")
+
+f = open(FILENAME, 'r')
+data = f.read()
+print(data)
+
+with open(FILENAME) as reader:
+    print(reader.read())
