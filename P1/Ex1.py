c61773893119aafbe3e36ffb8f1332f53f2dcbea
celiaserrano <c.serranos.2020@alumnos.urjc.es>
2021-03-08 23:41:52 +0100

    Practice 1

---
+++ P1/Ex1.py
@@ -0,0 +1,6 @@
+from Seq1 import Seq
+
+print("-----| Exercise 1 |------")
+seq = Seq("ACTGA")
+print("Sequence" + str(1) + ": (lenght:" + str(seq.len()) + ") " + str(seq))
+
---
+++ P1/Seq1.py
@@ -0,0 +1,50 @@
+import termcolor
+
+class Seq:
+    """A class for representing sequences"""
+
+    def __init__(self, strbases = "NULL"):
+
+        # Initialize the sequence with the value
+        # passed as argument when creating the object
+        self.strbases = strbases
+
+        if strbases == "NULL" :
+            print("NULL seq created")
+            self.strbases = strbases
+
+        else:
+
+            if self.is_valid_sequence():
+                print("New sequence created!")
+
+            else:
+                self.strbases = "Error"
+                print("INCORRECT Sequence detected")
+
+    def is_valid_sequence(self):
+        for c in self.strbases:
+            if c != "A" and c != "C" and c != "G" and c != "T":
+                return False
+        return True
+
+
+    @staticmethod
+    def print_seqs(list_sequences):
+        for i in range(0,len(list_sequences)):
+            text = "Sequence" + str(i) + ": (lenght:" + str(list_sequences[i].len()) + ") " + str(list_sequences[i])
+            termcolor.cprint(text, 'yellow')
+
+    def __str__(self):
+        """Method called when the object is being printed"""
+
+        # -- We just return the string with the sequence
+        return self.strbases
+
+    def len(self):
+        if self.strbases == "NULL" or self.strbases == "Error":
+            return 0
+
+        else:
+            return len(self.strbases)
+
