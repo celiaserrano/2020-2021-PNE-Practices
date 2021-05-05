08c2bc2d1c450d8eca3d5c9d7f82842dcfb806b6
celiaserrano <c.serranos.2020@alumnos.urjc.es>
2021-05-05 19:05:16 +0200

    PRACTICE 7

---
+++ P7/Seq1.py
@@ -0,0 +1,147 @@
+import termcolor
+from pathlib import Path
+
+class Seq:
+    """A class for representing sequences"""
+    NULL_SEQUENCE = "NULL"
+    INVALID_SEQUENCE = "ERROR"
+
+    def __init__(self, strbases = NULL_SEQUENCE):
+
+        # Initialize the sequence with the value
+        # passed as argument when creating the object
+        self.strbases = strbases
+
+        if strbases == Seq.NULL_SEQUENCE:
+            print("NULL seq created")
+            self.strbases = strbases
+
+        else:
+
+            if self.is_valid_sequence():
+                print("New sequence created!")
+
+            else:
+                self.strbases = Seq.INVALID_SEQUENCE
+                print("INCORRECT Sequence detected")
+
+    def is_valid_sequence(self):
+        for c in self.strbases:
+            if c != "A" and c != "C" and c != "G" and c != "T":
+                return False
+        return True
+
+
+    @staticmethod
+    def print_seqs(list_sequences):
+        for i in range(0,len(list_sequences)):
+            text = "Sequence" + str(i) + ": (lenght:" + str(list_sequences[i].len()) + ") " + str(list_sequences[i])
+            termcolor.cprint(text, 'yellow')
+
+    def __str__(self):
+        """Method called when the object is being printed"""
+
+        # -- We just return the string with the sequence
+        return self.strbases
+
+    def len(self):
+        if self.strbases == Seq.NULL_SEQUENCE or self.strbases == Seq.INVALID_SEQUENCE:
+            return 0
+
+        else:
+            return len(self.strbases)
+
+    def count_bases(self):
+        a, c, g, t = 0, 0, 0, 0
+        if self.strbases == Seq.NULL_SEQUENCE or self.strbases == Seq.INVALID_SEQUENCE:
+            return a, c, g, t
+        else:
+          for ch in self.strbases:
+            if ch == "A":
+                a += 1
+            elif ch == "C":
+                c += 1
+            elif ch == "G":
+                g += 1
+            else:
+                t += 1
+        return a, c, g, t
+
+    def count(self):
+        a, c, g, t = self.count_bases()
+
+        return {"A":a, "C":c, "G":g, "T":t}
+
+    def reverse(self):
+        if self.strbases == Seq.NULL_SEQUENCE:
+            return "NULL"
+        elif self.strbases == Seq.INVALID_SEQUENCE:
+            return "ERROR"
+        else:
+            return self.strbases[::-1]
+
+
+    def complement(self):
+        if self.strbases == Seq.NULL_SEQUENCE:
+            return "NULL"
+        elif self.strbases == Seq.INVALID_SEQUENCE:
+            return "ERROR"
+        else:
+            complement = ""
+            for ch in self.strbases:
+                if ch == "A":
+                    complement += "T"
+                elif ch == "C":
+                    complement += "G"
+                elif ch == "G":
+                    complement += "C"
+                elif "T":
+                    complement += "A"
+
+            return complement
+
+    @staticmethod
+    def take_out_first_line(seq):
+        return seq[seq.find("\n") + 1:].replace("\n", "")
+
+
+
+    def read_fasta(self, filename):
+        self.strbases = Seq.take_out_first_line(Path(filename).read_text())
+        print(self.strbases)
+
+
+    def most_freq_base(self):
+        a, c, g, t = self.count_bases()
+        if a > c and a > g and a > t:
+            return "Most frequent Base: A"
+
+        elif c > a and c > g and c > t:
+            return "Most frequent Base: C"
+
+        elif g > a and g > c and g > t:
+            return "Most frequent Base: G"
+
+        else:
+            return "Most frequent Base: T"
+
+
+    def percent(self):
+        a, c, g, t = self.count_bases()
+        A_p = (a / ( c + g + t)) * 100
+        C_p = (c / ( a + g + t)) * 100
+        G_p = (g / ( a + c + t)) * 100
+        T_p = ( t / ( a + c + g)) * 100
+        result = "\nA: " + str(a) + " (" + str(round(A_p, 2)) + "%)" + "\nC: " + str(c) + " (" + str(round(C_p, 2)) + "%)" + "\nG: " + str(g) + " (" + str(round(G_p, 2))  + "%)" + "\nT: " + str(t) + " (" + str(round(T_p, 2)) + "%)"
+        return result
+
+
+
+
+
+
+def test_sequences():
+    s1 = Seq()
+    s2 = Seq("ACTGA")
+    s3 = Seq("Invalid Sequence")
+    return s1, s2, s3
---
+++ P7/ex5.py
@@ -0,0 +1,46 @@
+import http.client
+import json
+import Seq1
+
+def print_color(message, color):
+    from termcolor import cprint, colored
+    print(colored(message, color))
+
+DICT_GENES = {
+       "FRAT1": "ENSG00000165879",
+    "ADA": "ENSG00000196839",
+    "FXN": "ENSG00000165060",
+     "RNU6-269P": "ENSG00000212379",
+     "MIR633": "ENSG00000207552",
+     "TTTY4": "ENSG00000226906",
+     "RBMY2YP": "ENSG00000227633",
+     "FGFR3": "ENSG00000068078",
+     "KDR": "ENSG00000128052",
+    "ANK2": "ENSG00000145362"
+}
+
+SERVER = "rest.ensembl.org"
+ENDPOINT = "/sequence/id/"
+ID = DICT_GENES["MIR633"]
+PARAMETERS = "?content-type=application/json"
+
+connection = http.client.HTTPConnection(SERVER)
+try:
+    for key, id in DICT_GENES.items():
+        connection.request("GET", ENDPOINT + id + PARAMETERS)
+        response = connection.getresponse()
+        if response.status == 200:
+            response_dict = json.loads(response.read().decode())
+            # print(json.dumps(response_dict, indent=4, sort_keys=True))
+            sequence = Seq1.Seq(response_dict["seq"])
+            s_lenght = sequence.len()
+            percents = sequence.percent()
+            freq_base = sequence.most_freq_base()
+            print_color("Gene: " , key, "yellow")
+            print_color("Total lenght: ", s_lenght, "yellow")
+            print_color(percents, "blue")
+            print_color(freq_base,"yellow")
+
+
+except KeyError:
+    print("The gene is not inside our dictionary,. Choose one of the following:", list(DICT_GENES.keys()))
